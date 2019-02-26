1c1a2e4eb80f3a6c7353aba7792de2ecf43f90b4
Cesar Augusto <cesarabruschetta@gmail.com>
2019-02-26 16:30:07 +0000

    Ajustes no requeriment e no admin

--- helpecBackend/hp_account/admin.py
+++ helpecBackend/hp_account/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from .models import HP_User
+from .models import HP_User, Contact, Occurrence
 
 # Register your models here.
 @admin.register(HP_User)
 class HP_UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Contact)
+class Contact(admin.ModelAdmin):
+    pass
+
+@admin.register(Occurrence)
+class Occurrence(admin.ModelAdmin):
     pass
